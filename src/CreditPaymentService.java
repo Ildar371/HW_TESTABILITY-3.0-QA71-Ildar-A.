8ebaf70f0c530d7c867a07feeb8b3e3fcc5b6a0e
ildar.a <ildar371@ya.ru>
2023-10-15 13:31:21 +0300

    correct formula

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,15 @@
 public class CreditPaymentService {
     public double calculate(double creditSumm, double percent, double monthsCount) {
 
-    double annCoeff;
-    double x1 = Math.pow(1 + percent, monthsCount);
-    annCoeff = ((percent*x1)/(x1-1));
-    double annPayment;
-    annPayment = creditSumm*annCoeff;
-    double result = annPayment;
+        double annCoeff;
+        double percentForFormula = percent / 12;
+        double percentForFormula2 = percentForFormula / 100;
+        double x1 = Math.pow((1 + percentForFormula2), monthsCount);
+        annCoeff = ((percentForFormula2 * x1) / (x1 - 1));
+        double annPayment;
+        annPayment = creditSumm * annCoeff;
+        double result = annPayment;
         return result;
 
-}
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double creditSumm = 1_000_000;
-        double percent = 0.0099;
+        double percent = 9.99;
         double monthsCount = 12;
         double everyMonthPayment = service.calculate(creditSumm, percent, monthsCount);
         System.out.println("Ежемесячный платеж: " + everyMonthPayment);
 
         creditSumm = 1_000_000;
-        percent = 0.0099;
+        percent = 9.99;
         monthsCount = 24;
         everyMonthPayment = service.calculate(creditSumm, percent, monthsCount);
         System.out.println("Ежемесячный платеж: " + everyMonthPayment);
 
         creditSumm = 1_000_000;
-        percent = 0.0099;
+        percent = 9.99;
         monthsCount = 36;
         everyMonthPayment = service.calculate(creditSumm, percent, monthsCount);
         System.out.println("Ежемесячный платеж: " + everyMonthPayment);
